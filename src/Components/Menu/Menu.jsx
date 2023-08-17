42d4bd89915a8582a2c23e7bb4e1a4c7ccc43b5c
Igor.K <igor.k.nsk@gmail.com>
2023-08-17 12:22:42 +0700

    recolor button

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -17,13 +17,13 @@ const Menu = React.memo(({ setActiveMenuHandler }) => {
 
         <Grid templateColumns={['1fr 1fr 1fr', '1fr 1fr 1fr 1fr']} gap={2}>
           <GridItem w='100%' h='10' rowSpan={1} >
-            <Button w={'100%'} backgroundColor={'#457b9d'} color={'white'} onClick={() => { setActiveMenuHandler('all') }} fontSize={['.8rem', '1rem']}>Все задачи</Button>
+            <Button w={'100%'} backgroundColor={'#006d77'} color={'white'} onClick={() => { setActiveMenuHandler('all') }} fontSize={['.8rem', '1rem']}>Все задачи</Button>
           </GridItem>
           <GridItem w='100%' h='10'  >
-            <Button w={'100%'} backgroundColor={'custom.red.100'} color={'white'} size='md' onClick={() => { setActiveMenuHandler('work') }} fontSize={['.8rem', '1rem']}>Сделать</Button>
+            <Button w={'100%'} backgroundColor={'#e07a5f'} color={'white'} size='md' onClick={() => { setActiveMenuHandler('work') }} fontSize={['.8rem', '1rem']}>Сделать</Button>
           </GridItem>
           <GridItem w='100%' h='10'  >
-            <Button w={'100%'} backgroundColor={'custom.yellow.100'} color={'white'} size='md' onClick={() => { setActiveMenuHandler('done') }} fontSize={['.8rem', '1rem']}>Завершенно</Button>
+            <Button w={'100%'} backgroundColor={'#8d99ae'} color={'white'} size='md' onClick={() => { setActiveMenuHandler('done') }} fontSize={['.8rem', '1rem']}>Завершенно</Button>
           </GridItem>
           <GridItem w={['30%', '100%']} m={'auto'} h='10' rowSpan={1} colSpan={['3', '1']} >
 
